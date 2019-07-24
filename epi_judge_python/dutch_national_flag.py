43218b4679786d4a315d6a3b1313d10f6765b4aa
Athena Liu <sdathena@mail.ubc.ca>
2019-07-24 16:07:11 -0700

    Do even_odd_array and dutch_national_flag problems

--- epi_judge_python/dutch_national_flag.py
+++ epi_judge_python/dutch_national_flag.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import functools
 
 from test_framework import generic_test
@@ -7,8 +8,45 @@ from test_framework.test_utils import enable_executor_hook
 RED, WHITE, BLUE = range(3)
 
 
-def dutch_flag_partition(pivot_index, A):
-    # TODO - you fill in here.
+def dutch_flag_partition(pivot_index: int, A: List[int]) -> None:
+    p = A[pivot_index]
+    METHOD = 2
+
+    if METHOD == 1:
+        # Method 1: O(n) time, O(1) space
+        # 1st: pivot <= items to the left, > items to the right
+        i, j = 0, len(A)
+        while i < j:
+            if A[i] <= p:
+                i += 1
+            else:
+                j -= 1
+                A[i], A[j] = A[j], A[i]
+
+        # 2nd: in the <= items subarray, pivot < items to the right
+        # = items to the right.
+        i = 0
+        while i < j:
+            if A[i] < p:
+                i += 1
+            else:
+                j -= 1
+                A[i], A[j] = A[j], A[i]
+
+    else:
+        # Method 2: Similar idea but pivot everything in one round
+        i, j, k = 0, 0, len(A)  # pointers for <, =, > items
+        while j < k:
+            if A[j] == p:
+                j += 1
+            elif A[j] < p:
+                A[i], A[j] = A[j], A[i]
+                i += 1
+                j += 1
+            else:
+                k -= 1
+                A[k], A[j] = A[j], A[k]
+
     return
 
 
@@ -43,3 +81,9 @@ if __name__ == '__main__':
         generic_test.generic_test_main("dutch_national_flag.py",
                                        'dutch_national_flag.tsv',
                                        dutch_flag_partition_wrapper))
+    # idx = 52
+    # A = [0, 0, 1, 2, 0, 0, 1, 0, 1, 2, 0, 0, 2, 2, 1, 0, 2, 0, 1, 1, 1, 0, 2, 1, 1, 1, 1, 1, 1, 1, 2,
+    #      1, 1, 2, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 2, 0, 0, 1, 1, 1, 1, 1, 2, 1, 0, 1, 2, 1, 2]
+    # print(A[52])
+    # dutch_flag_partition(idx, A)
+    # print(A)
