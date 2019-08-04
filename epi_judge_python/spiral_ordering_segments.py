4e29d645c1e23c206d7ae135a32cc406346c0bc3
Athena Liu <sdathena@mail.ubc.ca>
2019-08-03 18:09:46 -0700

    Do pb 5.18 spiral ordering

--- epi_judge_python/spiral_ordering_segments.py
+++ epi_judge_python/spiral_ordering_segments.py
@@ -2,8 +2,25 @@ from test_framework import generic_test
 
 
 def matrix_in_spiral_order(square_matrix):
-    # TODO - you fill in here.
-    return []
+    directions = ((0, 1), (1, 0), (0, -1), (-1, 0))
+    i = j = n_dir = 0
+    result = []
+    M = len(square_matrix)
+
+    # Improvement 1: change while to for loop
+    # while i < m and j < m and square_matrix[i][j] != 0:
+    for _ in range(M*M):
+        result.append(square_matrix[i][j])
+        square_matrix[i][j] = 0
+        next_i, next_j = i + directions[n_dir][0], j + directions[n_dir][1]
+        if (next_i >= M) or (next_j >= M) or (square_matrix[next_i][next_j] == 0):
+            n_dir = (n_dir + 1) % 4
+            # Improvement 2: change next_i/j only if changing direction
+            next_i, next_j = i + directions[n_dir][0], j + directions[n_dir][1]
+        # i, j = i + directions[n_dir][0], j + directions[n_dir][1]
+        i, j = next_i, next_j
+
+    return result
 
 
 if __name__ == '__main__':
