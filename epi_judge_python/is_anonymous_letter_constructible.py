9c38f646570c4a6597c830c290d23e2bb0b7470e
Athena Liu <sdathena@mail.ubc.ca>
2019-08-16 16:47:58 -0700

    Do anagrams and pb12.2 letter from magazine

--- epi_judge_python/is_anonymous_letter_constructible.py
+++ epi_judge_python/is_anonymous_letter_constructible.py
@@ -1,9 +1,32 @@
 from test_framework import generic_test
+from collections import Counter
 
 
 def is_letter_constructible_from_magazine(letter_text, magazine_text):
-    # TODO - you fill in here.
-    return True
+    char_in_letter_counts = Counter(letter_text)
+
+    # ==== Naive method: ====
+    # char_in_magazine_counts = Counter(magazine_text)
+
+    # for key in char_in_letter_counts:
+    #     if char_in_magazine_counts.get(key) is None or char_in_magazine_counts[key] < char_in_letter_counts[key]:
+    #         return False
+
+    # return True
+
+    # === Pythonic Naive method: ===
+    # return not (Counter(letter_text) - Counter(magazine_text))
+
+    # ==== More efficient method: ====
+    for c in magazine_text:
+        if char_in_letter_counts.get(c):
+            char_in_letter_counts[c] -= 1
+            if char_in_letter_counts[c] == 0:
+                del char_in_letter_counts[c]
+                if not char_in_letter_counts:
+                    return True
+
+    return False
 
 
 if __name__ == '__main__':
