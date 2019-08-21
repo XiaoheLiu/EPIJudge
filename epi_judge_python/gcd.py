5b0d07c4c905ba86f34c47580cdbe1dfd53434ea
Athena Liu <sdathena@mail.ubc.ca>
2019-08-21 16:54:42 -0700

    Do gcd (greatest common divisor)

--- epi_judge_python/gcd.py
+++ epi_judge_python/gcd.py
@@ -2,8 +2,14 @@ from test_framework import generic_test
 
 
 def gcd(x, y):
-    # TODO - you fill in here.
-    return 0
+    ## Assuming x, y can be any order
+    # if x * y == 0:
+    #     return x + y
+    # x, y = min(x, y), max(x, y)
+    # return gcd(x, (y - x) % x)
+
+    ## Assuming x >= y
+    return x if y == 0 else gcd(y, (x-y) % y)
 
 
 if __name__ == '__main__':
